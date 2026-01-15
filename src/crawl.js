6ab26cac17948c95282d078121d74aa3394d6c79
rudraa19 <rudra19206@gmail.com>
2026-01-15 22:39:58 +0530

    chore: create getURLsFromHTML and tests

--- src/crawl.js
+++ src/crawl.js
@@ -1,3 +1,29 @@
+import { JSDOM } from "jsdom";
+
+export function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const l of linkElements) {
+        if (l.href.slice(0, 1) === "/") {
+            try {
+                const urlObj = new URL(`${baseURL}${l.href}`);
+                urls.push(urlObj.href);
+            } catch (err) {
+                console.log(`Error with url: ${err.message}`)
+            }
+        } else {
+            try {
+                const urlObj = new URL(l.href);
+                urls.push(urlObj.href);
+            } catch (err) {
+                console.log(`Error with url: ${err.message}`)
+            }
+        }
+    }
+    return urls;
+}
+
 export function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@jest/globals"
-import { normalizeURL } from "../src/crawl.js"
+import { getURLsFromHTML, normalizeURL } from "../src/crawl.js"
 
+// normalizeURL tests
 test('normalizeURL strip', () => {
     const input = 'https://link.rudrax.dev/path';
     const actual = normalizeURL(input);
@@ -21,3 +22,72 @@ test('normalizeURL capitalize', () => {
     const expected = 'link.rudrax.dev/path';
     expect(actual).toEqual(expected);
 })
+
+// getURLsFromHTML tests
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://link.rudrax.dev/path/">
+            LINK.rudrax.dev
+        </a>
+    </body>
+</html>    
+`;
+    const inputBaseURL = "https://link.rudrax.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://link.rudrax.dev/path/"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/">
+            LINK.rudrax.dev
+        </a>
+    </body>
+</html>    
+`;
+    const inputBaseURL = "https://link.rudrax.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://link.rudrax.dev/path/"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://link.rudrax.dev/path1/">
+            LINK.rudrax.dev
+        </a>
+        <a href="/path2/">
+            LINK.rudrax.dev
+        </a>
+    </body>
+</html>    
+`;
+    const inputBaseURL = "https://link.rudrax.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://link.rudrax.dev/path1/", "https://link.rudrax.dev/path2/"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">
+            LINK.rudrax.dev
+        </a>
+    </body>
+</html>    
+`;
+    const inputBaseURL = "https://link.rudrax.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
+    expect(actual).toEqual(expected);
+})
+
