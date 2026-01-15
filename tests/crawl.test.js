72162fcb6e5a2b68eb5acba8d2ca9b2677d3f57f
rudraa19 <rudra19206@gmail.com>
2026-01-15 22:07:33 +0530

    chore: create normalizeURL and tests

--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,9 +1,23 @@
 import { test, expect } from "@jest/globals"
 import { normalizeURL } from "../src/crawl.js"
 
-test('normalizeURL', () => {
-    const input = '';
+test('normalizeURL strip', () => {
+    const input = 'https://link.rudrax.dev/path';
     const actual = normalizeURL(input);
-    const expected = '';
+    const expected = 'link.rudrax.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('normalizeURL strip traling slash', () => {
+    const input = 'https://link.rudrax.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'link.rudrax.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('normalizeURL capitalize', () => {
+    const input = 'https://LINK.rudrax.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'link.rudrax.dev/path';
     expect(actual).toEqual(expected);
 })
